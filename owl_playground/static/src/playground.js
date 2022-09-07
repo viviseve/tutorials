ff9af470da273dc52ad830cc71ae2f57813a5adb
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 11:36:41 +0200

    [Solution] 1.3 A Todo Component

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,9 +1,14 @@
 /** @odoo-module **/
 
 import { Counter } from "./counter/counter";
+import { Todo } from "./todo/todo";
 import { Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
-    static components = { Counter };
+    static components = { Counter, Todo };
+
+    setup() {
+        this.todo = { id: 3, description: "buy milk", done: false };
+    }
 }
