9dacda7fbd5470d39b32965c17cdb036c8cdf96b
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 21:16:22 +0200

    [Solution] 1.8. Toggling todos

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -2,11 +2,16 @@
 
 import { Component } from "@odoo/owl";
 
-export class Todo extends Component {}
+export class Todo extends Component {
+    onClick(ev) {
+        this.props.toggleState(this.props.id);
+    }
+}
 
 Todo.template = "owl_playground.Todo";
 Todo.props = {
     id: { type: Number },
     description: { type: String },
     done: { type: Boolean },
+    toggleState: { type: Function },
 };
