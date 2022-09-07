e87058909f57d26dc1f7635954e87ba83d2e3acb
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 13:24:45 +0200

    [Solution] 1.5. A List of todos

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,14 +1,10 @@
 /** @odoo-module **/
 
 import { Counter } from "./counter/counter";
-import { Todo } from "./todo/todo";
+import { TodoList } from "./todo_list/todo_list";
 import { Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
-    static components = { Counter, Todo };
-
-    setup() {
-        this.todo = { id: 3, description: "buy milk", done: false };
-    }
+    static components = { Counter, TodoList };
 }
---
+++ owl_playground/static/src/todo_list/todo_list.js
@@ -0,0 +1,18 @@
+/** @odoo-module */
+
+import { Todo } from "../todo/todo";
+
+import { Component } from "@odoo/owl";
+
+export class TodoList extends Component {
+    setup() {
+        this.todoList = [
+            { id: 3, description: "buy milk", done: false },
+            { id: 4, description: "buy eggs", done: true },
+            { id: 5, description: "buy avocado", done: true },
+        ];
+    }
+}
+
+TodoList.components = { Todo };
+TodoList.template = "owl_playground.TodoList";
