b3fe031d5c75ba9f9aa47466a8457a3c4b2e774f
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 13:17:35 +0200

    [Solution] 1.4. Props Validation

--- owl_playground/static/src/todo/todo.js
+++ owl_playground/static/src/todo/todo.js
@@ -5,3 +5,8 @@ import { Component } from "@odoo/owl";
 export class Todo extends Component {}
 
 Todo.template = "owl_playground.Todo";
+Todo.props = {
+    id: { type: Number },
+    description: { type: String },
+    done: { type: Boolean },
+};
