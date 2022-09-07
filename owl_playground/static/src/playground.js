e82400f64b1a5a741ff31a18f0409f1166e58f86
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 10:10:39 +0200

    [Solution] 1.1 Display a Counter

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -4,4 +4,12 @@ import { Component, useState } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
+
+    setup() {
+        this.state = useState({ value: 1 });
+    }
+
+    increment() {
+        this.state.value = this.state.value + 1;
+    }
 }
