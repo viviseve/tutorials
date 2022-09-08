49daab6b5663a5064b8f98a8395e5fec41fb9bcc
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-08 12:58:23 +0200

    [Solution] 1.11 Miscellaneous small tasks

--- owl_playground/static/src/card/card.js
+++ owl_playground/static/src/card/card.js
@@ -5,3 +5,12 @@ import { Component } from "@odoo/owl";
 export class Card extends Component {}
 
 Card.template = "owl_playground.Card";
+Card.props = {
+    slots: {
+        type: Object,
+        shape: {
+            default: Object,
+            title: { type: Object, optional: true },
+        },
+    },
+};
