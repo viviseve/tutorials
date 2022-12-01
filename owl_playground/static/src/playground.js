e1a6b98bbb74231e671167bfbb652cf91dd953e4
fdardenne <dafl@odoo.com>
2022-12-01 13:42:03 +0100

    [Solution] 1.2 Extract counter in a component

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,15 +1,9 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
-
-    setup() {
-        this.state = useState({ value: 1 });
-    }
-
-    increment() {
-        this.state.value = this.state.value + 1;
-    }
+    static components = { Counter };
 }
